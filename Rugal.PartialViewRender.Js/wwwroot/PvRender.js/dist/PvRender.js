add4b6f53af08b69134b344ab95375ab4b759804
Rugal <john2653589@gmail.com>
2024-01-23 20:03:59 +0800

    Rugal Update

--- Rugal.PartialViewRender.Js/wwwroot/PvRender.js/dist/PvRender.js
+++ Rugal.PartialViewRender.Js/wwwroot/PvRender.js/dist/PvRender.js
@@ -1,9 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
+    InitPvName();
+
     const AllPvSlot = document.querySelectorAll('[pv-slot]');
     AllPvSlot.forEach(PvSlot => {
         let PvName = PvSlot.getAttribute('pv-slot');
-        let PvNames = PvName.split('.');
-        SetPvTepmlate(PvNames, PvSlot);
+        SetPvSlot(PvName, PvSlot);
     });
 
     const AllPvAttr = document.querySelectorAll('[pv-attr]');
@@ -16,18 +17,23 @@ document.addEventListener('DOMContentLoaded', () => {
     AllPvSlot.forEach(Item => Item.remove());
     AllPvAttr.forEach(Item => Item.remove());
 });
-function SetPvTepmlate(PvNames, PvSlot) {
-    let LastName = PvNames.pop();
-    let AllQuerys = PvNames
-        .map(Item => `[pv-name="${Item}"]`);
-
-    AllQuerys.push(`[pv-template="${LastName}"]`);
-    let QueryResult = AllQuerys.join(' ');
+function SetPvSlot(PvName, PvSlot) {
+    let QueryResult = `[pv-name="${PvName}"]`;
     let FindPv = document.querySelector(QueryResult);
     if (FindPv == null)
         return;
 
     FindPv.innerHTML = PvSlot.innerHTML;
+    let InnerPv = FindPv.querySelectorAll('[pv-name]');
+    InnerPv.forEach(Dom => {
+        let ParentPvName = RCS_FindParentPvName(Dom);
+        if (ParentPvName == null)
+            return;
+
+        let DomPvName = Dom.getAttribute('pv-name');
+        let SetPvName = `${ParentPvName}.${DomPvName}`;
+        Dom.setAttribute('pv-name', SetPvName);
+    });
 }
 
 function SetPvAttr(PvNames, PvSlot) {
@@ -42,4 +48,36 @@ function SetPvAttr(PvNames, PvSlot) {
             continue;
         FindPv.setAttribute(Item.name, Item.value);
     }
+}
+
+function InitPvName() {
+    let AllPv = document.querySelectorAll('[pv-name]');
+    AllPv.forEach(Dom => {
+        let PvName = Dom.getAttribute('pv-name');
+        let SetPvName = RCS_CombinePvName(Dom, PvName);
+        Dom.setAttribute('pv-name', SetPvName);
+    });
+}
+
+function RCS_CombinePvName(Dom, PvName) {
+    if (Dom.parentElement.tagName == 'BODY')
+        return PvName;
+
+    let GetPvName = Dom.parentElement.getAttribute('pv-name');
+    if (GetPvName != null)
+        PvName = `${GetPvName}.${PvName}`;
+
+    return RCS_CombinePvName(Dom.parentElement, PvName);
+}
+
+function RCS_FindParentPvName(Dom) {
+    let GetParen = Dom.parentElement;
+    let ParenPvName = GetParen.getAttribute('pv-name');
+    if (ParenPvName != null)
+        return ParenPvName;
+
+    if (GetParen.tagName == 'BODY')
+        return null;
+
+    return RCS_FindParentPvName(Dom.parentElement);
 }
