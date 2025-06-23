c4191c6560f67ac5b524d9fd9b3e9403b0c87a9d
Rugal Tu <john2653589@gmail.com>
2025-06-23 11:02:43 +0800

    update bat

--- Rugal.PartialViewRender/release.bat
+++ Rugal.PartialViewRender/release.bat
@@ -1,5 +1,5 @@
 
 dotnet build
-dotnet nuget push -s https://nuget.dtvl.com.tw -k %NUGET_API_KEY% bin/Debug/Rugal.PartialViewRender.1.1.3.nupkg
+dotnet nuget push -s https://nuget.dtvl.com.tw -k %NUGET_API_KEY% bin/Debug/Rugal.PartialViewRender.1.2.0.nupkg
 
 pause
